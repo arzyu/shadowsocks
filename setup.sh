aae067eee668658a3083e1508eea909b267c74e4
arzyu <arzyu@live.cn>
2019-07-29 06:14:18 +0800

    refactor: new setup script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,101 @@
 #! /bin/bash
 
-set -e
-
-cd "$(dirname "$0")"
-
-base_dir="$PWD"
-
 systemd=/etc/systemd/system
+service_name=shadowsocks.service
+service_file="$systemd/$service_name"
 
-cat > "$systemd/shadowsocks.service" << EOT
-[Unit]
-Description=shadowsocks
-Requires=docker.service
-After=docker.service
+function set_port() {
+	local default_port=8388
 
-[Service]
-Restart=Always
-ExecStart=$(which docker-compose) --file "$base_dir/docker-compose.yml" up --force-recreate
-ExecStop=$(which docker-compose) --file "$base_dir/docker-compose.yml" down
+	printf "> Set port (default: $default_port): " > /dev/tty
+	read port < /dev/tty
+	printf "${port:-$default_port}"
+}
 
-[Install]
-WantedBy=multi-user.target
+function set_password() {
+	local password=
 
-EOT
+	while [[ -z "$password" ]]; do
+		printf "> Set password: " > /dev/tty
+		read password < /dev/tty
+	done
 
-systemctl daemon-reload
-systemctl enable shadowsocks.service
-systemctl start shadowsocks.service
+	printf "$password"
+}
+
+function add_systemd() {
+	local port="$(set_port)"
+	local password="$(set_password)"
+
+	if [ -f "$service_file" ]; then
+		systemctl stop "$service_name"
+		systemctl disable "$service_name" > /dev/null 2>&1
+	fi
+
+	cat > "$service_file" <<-EOT
+		[Unit]
+		Description=shadowsocks
+		Requires=docker.service
+		After=docker.service
+
+		[Service]
+		Restart=Always
+		ExecStart=$(which docker) run --rm --name %n -p "$port:8388" -e "PASSWORD=$password" shadowsocks/shadowsocks-libev
+		ExecStop=$(which docker) stop %n
+
+		[Install]
+		WantedBy=multi-user.target
+	EOT
+
+	systemctl daemon-reload
+	systemctl enable "$service_name" > /dev/null 2>&1
+	systemctl start "$service_name"
+}
+
+function remove_systemd() {
+	systemctl stop "$service_name"
+	systemctl disable "$service_name" > /dev/null 2>&1
+
+	rm "$service_file"
+
+	systemctl daemon-reload
+
+	printf "> Shadowsocks has been removed.\n"
+}
+
+function check_service() {
+	local times=0
+
+	while (( times < 5 )); do
+		sleep 1
+		if [ -n "$(docker ps --filter "name=$service_name" --format "{{.Names}}")" ]; then
+			break
+		fi
+		(( times++ ))
+	done
+
+	if (( times < 5 )); then
+		printf "\n> Shadowsocks is running:\n"
+		docker ps --filter "name=$service_name" --format "table {{.Names}}\t{{.Image}}\t{{.Ports}}"
+		printf "\n"
+	else
+		printf "\n> Service startup timeout. Run \"docker ps -a\" and have a look.\n"
+	fi
+}
+
+function main() {
+	if [[ "$@" == "--remove" ]]; then
+		remove_systemd
+		exit
+	fi
+
+	if [ ! -x "$(command -v docker)" ]; then
+		# install docker
+		curl -fsSL get.docker.com | sh
+	fi
+
+	add_systemd
+	check_service
+}
+
+main "$@"
